fd93408c53bedbfbb3ce0fd4093e30f11fe3b716
Maizares, Juan <juan_maizares386@hotmail.com>
2022-05-20 12:17:47 -0300

    form de formulario para suscripcion

---
+++ blog/applications/home/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from .models import Suscribers
+
+class SuscribersForm(forms.ModelForm):
+    class Meta:
+        model = Suscribers
+        fields = ('email',)
+        widgets = {
+            'email': forms.EmailInput(
+                attrs={
+                    'placeholder': 'Su correo electronico...'
+                }
+            ),
+        }
+        
